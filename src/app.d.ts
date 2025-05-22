4969cf73a8bb87717330ee89955faf821b9223ca
nasheomirro <76786181+nasheomirro@users.noreply.github.com>
2025-05-22 13:53:33 +0800

    added unplugin-icons

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+import 'unplugin-icons/types/svelte.d.ts';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import icons from "unplugin-icons/vite";
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), icons({ compiler: "svelte" })]
 });
